77c10503e515f91bad2a273cc5feb6936e9597b6
CainLuo <350116542@qq.com>
2021-07-01 00:58:40 +0800

    Update JustifyContentController.swift

--- TextureExamples/TextureExamples/Modules/JustifyContent/JustifyContentController.swift
+++ TextureExamples/TextureExamples/Modules/JustifyContent/JustifyContentController.swift
@@ -9,8 +9,12 @@ import AsyncDisplayKit
 
 class JustifyContentController: ASDKViewController<ASDisplayNode> {
     
+    private let roundNode = JustifyContentRoundNode()
+    
     init(_ type: ASStackLayoutJustifyContent) {
         super.init(node: ASDisplayNode())
+        
+        configBackgroundNode()
     }
     
     required init?(coder: NSCoder) {
@@ -20,5 +24,24 @@ class JustifyContentController: ASDKViewController<ASDisplayNode> {
     override func viewDidLoad() {
         super.viewDidLoad()
         node.backgroundColor = .white
+    }
+}
+
+extension JustifyContentController {
+    private func configBackgroundNode() {
+        
+        node.addSubnode(roundNode)
+        
+        node.layoutSpecBlock = { [weak self] node, constrainedSize in
+            guard let roundNode = self?.roundNode else { return ASLayoutSpec() }
+            
+            roundNode.style.preferredSize = CGSize(width: 100, height: 100)
+            
+            let stack = ASStackLayoutSpec.horizontal()
+            stack.child = roundNode
+            stack.justifyContent = .end
+            
+            return stack
+        }
     }
 }
