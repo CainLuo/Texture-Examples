80de87cb696ffb987df6c6743e3570ec2052d551
CainLuo <350116542@qq.com>
2021-07-02 18:32:20 +0800

    Update AppDelegate.swift

--- TextureExamples/TextureExamples/AppDelegate.swift
+++ TextureExamples/TextureExamples/AppDelegate.swift
@@ -14,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                
+        let tabBarController = TabBarController()
+        tabBarController.tabBar.isTranslucent = false
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
 
         return true
