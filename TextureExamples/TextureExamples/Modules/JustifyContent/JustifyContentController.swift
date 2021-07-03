ec73edcdd52040bac1cd8f59ec30c425a0e8cd6d
CainLuo <350116542@qq.com>
2021-07-03 14:04:22 +0800

    Update JustifyContentController.swift

--- TextureExamples/TextureExamples/Modules/JustifyContent/JustifyContentController.swift
+++ TextureExamples/TextureExamples/Modules/JustifyContent/JustifyContentController.swift
@@ -10,10 +10,12 @@ import AsyncDisplayKit
 class JustifyContentController: ASDKViewController<ASDisplayNode> {
     
     private let roundNode = JustifyContentRoundNode()
+    private let squareNode = JustifyContentSquareNode()
+    private let infoNode = InfoNode()
+    private var isShowAnimated = false
     
     init(_ type: ASStackLayoutJustifyContent) {
         super.init(node: ASDisplayNode())
-
         configBackgroundNode()
     }
     
@@ -23,6 +25,7 @@ class JustifyContentController: ASDKViewController<ASDisplayNode> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        infoNode.delegate = self
         node.backgroundColor = .white
     }
 }
@@ -31,20 +34,44 @@ extension JustifyContentController {
     private func configBackgroundNode() {
 
         node.addSubnode(roundNode)
+        node.addSubnode(squareNode)
+        node.addSubnode(infoNode)
 
         node.layoutSpecBlock = { [weak self] node, constrainedSize in
-            guard let roundNode = self?.roundNode else { return ASLayoutSpec() }
-            
+            guard let roundNode = self?.roundNode,
+                  let squareNode = self?.squareNode,
+                  let infoNode = self?.infoNode else {
+                return ASLayoutSpec()
+            }
+                                                
+            squareNode.style.preferredSize = CGSize(width: 100, height: 100)
+            squareNode.cornerRadius = 50
             roundNode.style.preferredSize = CGSize(width: 100, height: 100)
-            
-            let stack = ASStackLayoutSpec.horizontal()
-            stack.child = roundNode
-            stack.justifyContent = .end
 
-            return ASCenterLayoutSpec(horizontalPosition: .end,
-                                      verticalPosition: .start,
-                                      sizingOption: [],
-                                      child: stack)
+            infoNode.style.preferredSize = CGSize(width: constrainedSize.max.width, height: 300)
+            infoNode.style.spacingBefore = 100
+
+            let squareStack = ASStackLayoutSpec.horizontal()
+            squareStack.child = squareNode
+            squareStack.justifyContent = self?.isShowAnimated == false ? .start : .end
+            
+            let roundStack = ASStackLayoutSpec.horizontal()
+            roundStack.child = roundNode
+            roundStack.justifyContent = .end
+            
+            let background = ASBackgroundLayoutSpec(child: squareStack, background: roundStack)
+
+            let vstack = ASStackLayoutSpec.vertical()
+            vstack.children = [background, infoNode]
+            return vstack
         }
+    }
+}
+
+// MARK: - InfoNodeDelegate
+extension JustifyContentController: InfoNodeDelegate {
+    func infoNodeDidSelectDefine(_ node: InfoNode, isReset: Bool) {
+        isShowAnimated = !isReset
+        self.node.setNeedsLayout()
     }
 }
