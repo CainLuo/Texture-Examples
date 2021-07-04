69a30f16843e2c55bc6129ab3cda20c721d0f8b0
CainLuo <350116542@qq.com>
2021-07-04 23:39:01 +0800

    Create SquareNode.swift

---
+++ TextureExamples/TextureExamples/Base/SquareNode.swift
@@ -0,0 +1,19 @@
+//
+//  SquareNode.swift
+//  TextureExamples
+//
+//  Created by CainLuo on 2021/7/4.
+//
+
+import AsyncDisplayKit
+
+// MARK: - SquareNode
+class SquareNode: ASDisplayNode {
+    
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        backgroundColor = .green
+    }
+}
