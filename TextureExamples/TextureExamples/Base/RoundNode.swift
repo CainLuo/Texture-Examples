374fb59a9fa422f2994897b83e0de80506e0c042
CainLuo <350116542@qq.com>
2021-07-04 23:38:59 +0800

    Create RoundNode.swift

---
+++ TextureExamples/TextureExamples/Base/RoundNode.swift
@@ -0,0 +1,18 @@
+//
+//  RoundNode.swift
+//  TextureExamples
+//
+//  Created by CainLuo on 2021/7/4.
+//
+
+import AsyncDisplayKit
+
+class RoundNode: ASDisplayNode {
+    // MARK: - RoundNode
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        backgroundColor = .yellow
+    }
+}
