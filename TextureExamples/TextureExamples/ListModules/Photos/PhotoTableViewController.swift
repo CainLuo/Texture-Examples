0fe5c4be5779d6f1b8e8a52d55428e37d85c1ac9
CainLuo <350116542@qq.com>
2021-07-06 00:08:28 +0800

    Update PhotoTableViewController.swift

--- TextureExamples/TextureExamples/ListModules/Photos/PhotoTableViewController.swift
+++ TextureExamples/TextureExamples/ListModules/Photos/PhotoTableViewController.swift
@@ -6,26 +6,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class PhotoTableViewController: UITableViewController {
+    
+    private let disposeBag = DisposeBag()
+    private let viewModel: PhotoTableViewModelTypes = PhotoTableViewModel()
+    private var dataSource: [PhotoTableModel] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.inputs.viewWillAppear.onNext(())
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
+        tableView.allowsSelection = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
+    
+    func bindViewModel() {
+        viewModel.outputs.items
+            .drive(onNext: { [weak self] items in
+                items.forEach { item in
+                    if self?.dataSource.filter({ $0.id == item.id }).isEmpty == true {
+                        self?.dataSource.append(item)
+                    }
+                    self?.tableView.reloadData()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
 // MARK: - UITableViewDataSource
 extension PhotoTableViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        dataSource.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        return cell
     }
 }
 
