458aa5b9cb9a6189af821e86ecf845da28106196
CainLuo <350116542@qq.com>
2021-07-07 00:12:05 +0800

    Create PhotoIGCollectionNodeController.swift

---
+++ TextureExamples/TextureExamples/ListModules/CollectionNode/IGListKit/PhotoIGCollectionNodeController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoIGCollectionNodeController.swift
+//  TextureExamples
+//
+//  Created by CainLuo on 2021/7/7.
+//
+
+import AsyncDisplayKit
+
+class PhotoIGCollectionNodeController: BaseCollectionNodeController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
