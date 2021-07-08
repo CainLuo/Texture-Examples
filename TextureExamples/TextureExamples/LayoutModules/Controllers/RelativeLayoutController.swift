eb5fbac2ed60503363c244d511e7af57f40aa3cd
CainLuo <350116542@qq.com>
2021-07-08 11:11:24 +0800

    Update RelativeLayoutController.swift

--- TextureExamples/TextureExamples/LayoutModules/Controllers/RelativeLayoutController.swift
+++ TextureExamples/TextureExamples/LayoutModules/Controllers/RelativeLayoutController.swift
@@ -11,11 +11,40 @@ class RelativeLayoutController: BaseNodeController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configBackgroundNode()
+        node.setNeedsLayout()
     }
 }
 
-// MARK: - RelativeContentNode
+extension RelativeLayoutController {
+    private func configBackgroundNode() {
+        let contentNode = RelativeContentNode()
+
+        node.addSubnode(contentNode)
+
+        node.layoutSpecBlock = { node, constrainedSize in
+            ASInsetLayoutSpec(insets: UIEdgeInsets(top: 100, left: 20, bottom: 100, right: 20), child: contentNode)
+        }
+    }
+}
+
+// MARK: -  RelativeContentNode
 class RelativeContentNode: ASDisplayNode {
 
+    override init() {
+        super.init()
+        automaticallyManagesSubnodes = true
+    }
+
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let backgroundNode = ColorNode(UIColor.blue)
+        let foregroundNode = ColorNode(UIColor.red, size: CGSize(width: 70.0, height: 100.0))
+
+        let relativeSpec = ASRelativeLayoutSpec(horizontalPosition: .start,
+                                                verticalPosition: .start,
+                                                sizingOption: [],
+                                                child: foregroundNode)
+
+        return ASBackgroundLayoutSpec(child: relativeSpec, background: backgroundNode)
+    }
 }
