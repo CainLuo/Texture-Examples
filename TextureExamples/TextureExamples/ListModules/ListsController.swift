fa0ec32b6cd72ee418ca1ba75027a0127122d0e3
CainLuo <350116542@qq.com>
2021-07-09 14:42:26 +0800

    Remove codes

--- TextureExamples/TextureExamples/ListModules/ListsController.swift
+++ TextureExamples/TextureExamples/ListModules/ListsController.swift
@@ -24,7 +24,6 @@ class ListsController: BaseTableNodeController {
         node.allowsSelection = true
         node.dataSource = self
         node.delegate = self
-        node.leadingScreensForBatching = 2.5
         bindViewModel()
     }
     
