063799139c27e102b48fd4938f7776245e5ef777
CainLuo <350116542@qq.com>
2021-07-12 23:57:10 +0800

    Update ScrollNodeController.swift

--- TextureExamples/TextureExamples/ASUIModules/Controllers/ScrollNodeController.swift
+++ TextureExamples/TextureExamples/ASUIModules/Controllers/ScrollNodeController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 import AsyncDisplayKit
+import TextureSwiftSupport
 
 class ScrollNodeController: BaseNodeController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configBackgroundNode()
+        node.setNeedsLayout()
+    }
+}
 
+extension ScrollNodeController {
+    private func configBackgroundNode() {
+        let contentNode = ScrollContentNode()
+        node.addSubnode(contentNode)
+        node.layoutSpecBlock = { node, constrainedSize in
+            ASWrapperLayoutSpec(layoutElement: contentNode)
+        }
+    }
+}
+
+// MARK: - ScrollContentNode
+class ScrollContentNode: ASScrollNode {
+    let imageNode = ASImageNode()
+    
+    override init() {
+        super.init()
+        automaticallyManagesSubnodes = true
+        automaticallyManagesContentSize = true
+
+        imageNode.image = #imageLiteral(resourceName: "image2")
+        imageNode.style.flexShrink = 1.0
+        imageNode.style.preferredSize = CGSize(width: UIScreen.main.bounds.size.height * 2,
+                                               height: UIScreen .main.bounds.size.height * 2)
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        LayoutSpec {
+            WrapperLayout {
+                imageNode
+            }
+        }
     }
 }
