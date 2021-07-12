16f363b6d325786941efc56073cd81593faefddb
CainLuo <350116542@qq.com>
2021-07-12 11:52:52 +0800

    Update ASUIListsViewModel.swift

--- TextureExamples/TextureExamples/ASUIModules/ASUIListsViewModel.swift
+++ TextureExamples/TextureExamples/ASUIModules/ASUIListsViewModel.swift
@@ -53,6 +53,7 @@ private func dataSource() -> [ASUIListsModel] {
         ASUIListsModel(title: "ASNetworkImageNode", type: .networkImage),
         ASUIListsModel(title: "ASTextNode", type: .text),
         ASUIListsModel(title: "ASButtonNode", type: .button),
-        ASUIListsModel(title: "ASMapNode", type: .map)
+        ASUIListsModel(title: "ASMapNode", type: .map),
+        ASUIListsModel(title: "ASEditableTextNode", type: .editableText)
     ]
 }
