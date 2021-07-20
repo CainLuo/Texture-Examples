d685fc28bf1617cb9a7f5973362a8df453d3637b
CainLuo <350116542@qq.com>
2021-07-20 12:01:31 +0800

    Update NinePatternsCellNode.swift

--- TextureExamples/TextureExamples/ASUIModules/NinePatterns/NinePatternsCellNode.swift
+++ TextureExamples/TextureExamples/ASUIModules/NinePatterns/NinePatternsCellNode.swift
@@ -14,9 +14,11 @@ class NinePatternsCellNode: ASCellNode {
     
     private let avatarImageNode = ASNetworkImageNode()
     private let nameNode = ASTextNode()
+    private var images: [String] = []
     
     init(_ item: NinePatternsModel) {
         super.init()
+        images = item.image
         automaticallyManagesSubnodes = true
         avatarImageNode.url = URL(string: item.avatar ?? "")
         avatarImageNode.style.preferredSize = CGSize(width: 40, height: 40)
@@ -34,14 +36,37 @@ extension NinePatternsCellNode {
                         avatarImageNode
                         nameNode
                     }
-                    configNicePatternsNodes()
+                    configNicePatternsNodes(constrainedSize.max)
                 }
             }
         }
     }
-    
-    /// FIXME: Not Finish Logic
-    private func configNicePatternsNodes() -> ASLayoutSpec {
-        ASLayoutSpec()
+
+    /// Config Nice Image Layout Spec
+    /// - Parameter size: CGSize, Node constrainedSize max size
+    /// - Returns: ASLayoutSpec
+    private func configNicePatternsNodes(_ size: CGSize) -> ASLayoutSpec {
+        var nodes: [ASDisplayNode] = []
+
+        for index in 0..<images.count {
+            let colCount = 3
+            let width = size.width / CGFloat(colCount)
+            let row = index / colCount
+            let col = index % colCount
+            let pointX = width * CGFloat(col)
+            let pointY = width * CGFloat(row)
+
+            let node = ASNetworkImageNode()
+            node.setURL(URL(string: images[index]), resetToDefault: true)
+            node.style.preferredSize = CGSize(width: width, height: width)
+            node.style.layoutPosition = CGPoint(x: pointX, y: pointY)
+            nodes.append(node)
+        }
+
+        return  LayoutSpec {
+            AbsoluteLayout {
+                nodes
+            }
+        }
     }
 }
