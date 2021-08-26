79b2e75e990c480185f6d509569ab348672c3b27
CainLuo <350116542@qq.com>
2021-08-26 11:12:25 +0800

    Update NSObject+ListDiffable.swift

--- TextureExamples/TextureExamples/Extensions/NSObject+ListDiffable.swift
+++ TextureExamples/TextureExamples/Extensions/NSObject+ListDiffable.swift
@@ -10,11 +10,11 @@ import IGListKit
 
 // MARK: - ListDiffable
 extension NSObject: ListDiffable {
-  public func diffIdentifier() -> NSObjectProtocol {
-    return self
-  }
-
-  public func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
-    return isEqual(object)
-  }
+    public func diffIdentifier() -> NSObjectProtocol {
+        return self
+    }
+    
+    public func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        return isEqual(object)
+    }
 }
