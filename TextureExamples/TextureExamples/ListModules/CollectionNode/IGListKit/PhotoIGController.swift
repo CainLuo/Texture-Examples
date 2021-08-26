b2316b7d01e2553edfcb486939e5561183943907
CainLuo <350116542@qq.com>
2021-08-26 11:12:28 +0800

    Update PhotoIGController.swift

--- TextureExamples/TextureExamples/ListModules/CollectionNode/IGListKit/PhotoIGController.swift
+++ TextureExamples/TextureExamples/ListModules/CollectionNode/IGListKit/PhotoIGController.swift
@@ -11,11 +11,49 @@ import IGListKit
 import AsyncDisplayKit
 
 class PhotoIGController: ListSectionController, ASSectionController {
-    private var photoModel: PhotoTableModel!
     
-    func nodeBlockForItem(at index: Int) -> ASCellNodeBlock {
-        let item = photoModel!
-        return { PhotoTableCellNode(item) }
+    private let disposeBag = DisposeBag()
+    private var photoSection: PhotoIGSectionsModel!
+    private let viewModel: PhotoTableViewModelTypes = PhotoTableViewModel()
+    private var batchContext: ASBatchContext?
+
+    override init() {
+        super.init()
+        
+        viewModel.outputs.items
+            .drive(onNext: { [weak self] items in
+                self?.reloadDataSource(items)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func reloadDataSource(_ items: [PhotoTableModel]) {
+        if !items.isEmpty,
+           let context = collectionContext {
+            
+            let tempItems = photoSection.photos + items
+            
+            context.performBatch(animated: true) { [weak self] context in
+                guard let weakSelf = self else {
+                    return
+                }
+                
+                let resultIndexSet = ListDiff(oldArray: weakSelf.photoSection.photos, newArray: tempItems, option: .equality)
+                context.insert(in: weakSelf, at: resultIndexSet.inserts)
+                context.delete(in: weakSelf, at: resultIndexSet.deletes)
+                weakSelf.photoSection.photos.append(contentsOf: items)
+            }
+            
+            batchContext?.completeBatchFetching(true)
+        }
+    }
+}
+
+// MARK: Fetch Photos
+extension PhotoIGController {
+    func beginBatchFetch(with context: ASBatchContext) {
+        batchContext = context
+        viewModel.inputs.fetchList()
     }
 }
 
@@ -32,6 +70,24 @@ extension PhotoIGController {
     }
     
     override func didUpdate(to object: Any) {
-        photoModel = object as? PhotoTableModel
+        if let section = object as? PhotoIGSectionsModel {
+            photoSection = section
+        }
+    }
+    
+    override func didSelectItem(at index: Int) {
+        log.debug(index)
+    }
+}
+
+// MARK: - Data Source Method
+extension PhotoIGController {
+    override func numberOfItems() -> Int {
+        photoSection.photos.count
+    }
+    
+    func nodeBlockForItem(at index: Int) -> ASCellNodeBlock {
+        let item = photoSection.photos[index]
+        return { PhotoTableCellNode(item) }
     }
 }
