ec45e7fdea90e3ff86a12d2ad432718d315224a2
CainLuo <350116542@qq.com>
2021-08-31 16:17:40 +0800

    Create PhotoIGHeaderController.swift

---
+++ TextureExamples/TextureExamples/ListModules/CollectionNode/IGListKit/PhotoIGHeaderController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoIGHeaderController.swift
+//  TextureExamples
+//
+//  Created by YYKJ0048 on 2021/8/30.
+//
+
+import RxSwift
+import RxCocoa
+import IGListKit
+import AsyncDisplayKit
+import TextureSwiftSupport
+
+class PhotoIGHeaderController: ListSectionController, ASSectionController {
+}
+
+// MARK: IGListKit override methods
+extension PhotoIGHeaderController {
+    override func cellForItem(at index: Int) -> UICollectionViewCell {
+        let cell = ASIGListSectionControllerMethods.cellForItem(at: index, sectionController: self)
+        return cell
+    }
+    
+    override func sizeForItem(at index: Int) -> CGSize {
+        let size = ASIGListSectionControllerMethods.sizeForItem(at: index)
+        return size
+    }
+    
+    override func didUpdate(to object: Any) {
+    }
+    
+    override func didSelectItem(at index: Int) {
+        log.debug(index)
+    }
+}
+
+// MARK: - Data Source Method
+extension PhotoIGHeaderController {
+    override func numberOfItems() -> Int {
+        1
+    }
+    
+    func nodeBlockForItem(at index: Int) -> ASCellNodeBlock {
+        { PhotoIGOtherCellNode("PhotoIG_Header".localized()) }
+    }
+}
